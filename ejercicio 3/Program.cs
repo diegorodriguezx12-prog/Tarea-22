1f3d68537a976fed9c38e566cee6bf158987ee5a
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-19 15:19:25 -0600

    ejercicio 3 terminado

--- ejercicio 3/Program.cs
+++ ejercicio 3/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿Console.Write("Ingrese la cantidad de filas: ");
+int filas = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese la cantidad de columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[filas, columnas];
+int sumaTotal = 0;
+
+for (int f = 0; f < filas; f++)
+{
+    for (int c = 0; c < columnas; c++)
+    {
+        Console.Write("Ingrese el valor para [" + f + "," + c + "]: ");
+        matriz[f, c] = int.Parse(Console.ReadLine());
+        sumaTotal = sumaTotal + matriz[f, c];
+    }
+}
+
+Console.WriteLine("\nMatriz ingresada:");
+for (int f = 0; f < filas; f++)
+{
+    for (int c = 0; c < columnas; c++)
+    {
+        Console.Write(matriz[f, c] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nLa suma total de todos los elementos es: " + sumaTotal);
