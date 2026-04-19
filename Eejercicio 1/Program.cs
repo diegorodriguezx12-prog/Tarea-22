47ea44dffcf196f5db6cfcd2b6c95e11bf9c9f6b
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-19 13:32:30 -0600

    ejercicio 1 terminado

--- Eejercicio 1/Program.cs
+++ Eejercicio 1/Program.cs
@@ -1,1 +1,26 @@
-﻿.
+﻿Console.Write("Ingrese la cantidad de filas: ");
+int filas = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese la cantidad de columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[filas, columnas];
+
+for (int f = 0; f < filas; f++)
+{
+    for (int c = 0; c < columnas; c++)
+    {
+        Console.Write("Ingrese el valor para [" + f + "," + c + "]: ");
+        matriz[f, c] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine();
+for (int f = 0; f < filas; f++)
+{
+    for (int c = 0; c < columnas; c++)
+    {
+        Console.Write(matriz[f, c] + "\t");
+    }
+    Console.WriteLine();
+}
