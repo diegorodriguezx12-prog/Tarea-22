c305992a420dd34ec766726da462692c23b80cfa
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-19 14:27:28 -0600

    ejercicio 2 terminado

--- ejercicio 2/Program.cs
+++ ejercicio 2/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿Console.Write("Ingrese la cantidad de filas: ");
+int filas = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese la cantidad de columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[filas, columnas];
+
+for (int f = 0; f < filas; f++)
+{
+    for (int c = 0; c < columnas; c++)
+    {
+        Console.Write("Ingrese el valor para [" + f + "," + c + "]: ");
+        matriz[f, c] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.Write("\n¿Qué fila desea ver? (0 a " + (filas - 1) + "): ");
+int filaSeleccionada = int.Parse(Console.ReadLine());
+
+if (filaSeleccionada >= 0 && filaSeleccionada < filas)
+{
+    Console.WriteLine("Valores de la fila " + filaSeleccionada + ":");
+    for (int c = 0; c < columnas; c++)
+    {
+        Console.Write(matriz[filaSeleccionada, c] + "\t");
+    }
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine("Esa fila no existe.");
+}
