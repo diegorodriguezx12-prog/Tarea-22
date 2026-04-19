d1d9c0b42691042f3c564f4ac1a1ef7eb23ff405
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-04-19 15:51:18 -0600

    ejercicio 4 terminado

--- ejercicio 4/Program.cs
+++ ejercicio 4/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿Console.Write("Ingrese la cantidad de filas: ");
+int filas = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese la cantidad de columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[filas, columnas];
+
+for (int f = 0; f < filas; f++)
+{
+    for (int c = 0; c < columnas; c++)
+    {
+        Console.Write("Ingrese el valor para [" + f + "," + c + "]: ");
+        matriz[f, c] = int.Parse(Console.ReadLine());
+    }
+}
+
+int mayor = matriz[0, 0];
+
+for (int f = 0; f < filas; f++)
+{
+    for (int c = 0; c < columnas; c++)
+    {
+        if (matriz[f, c] > mayor)
+        {
+            mayor = matriz[f, c];
+        }
+    }
+}
+
+Console.WriteLine("\nLa matriz es:");
+for (int f = 0; f < filas; f++)
+{
+    for (int c = 0; c < columnas; c++)
+    {
+        Console.Write(matriz[f, c] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nEl número mayor encontrado es: " + mayor);
